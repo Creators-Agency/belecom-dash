6105309a49bfa9cbbb9e234e3211e4999c7de8dd
Kevin Kayisire <kaykevin1@gmail.com>
2020-09-16 17:12:57 +0200

    Frontend scaffloding

--- resources/views/css.blade.php
+++
@@ -1,5 +0,0 @@
-<link href="{{asset('assets/libs/chartist/dist/chartist.min.css')}}" rel="stylesheet">
-<link href="{{asset('assets/extra-libs/c3/c3.min.css')}}" rel="stylesheet">
-<link href="{{asset('assets/extra-libs/jvector/jquery-jvectormap-2.0.2.css')}}" rel="stylesheet" />
-<!-- Custom CSS -->
-<link href="{{asset('dist/css/style.min.css')}}" rel="stylesheet">
