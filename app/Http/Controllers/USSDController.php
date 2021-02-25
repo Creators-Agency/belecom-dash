1a0d463d1fe591523f90e7f70e6f3e18bb669acc
tinoxn <niyonshutivalentin@gmail.com>
2021-02-25 21:15:03 +0200

    fixing bug

--- app/Http/Controllers/USSDController.php
+++ app/Http/Controllers/USSDController.php
@@ -330,10 +330,11 @@ class USSDController extends Controller {
                                 $this->stop($content, $sessionId);
                             }
 
+                        }else{
+                            $content  = "Amafaranga ntagomba kuba ubusa\n";
+                            $content .= "Murakoze!";
+                            $this->stop($content, $sessionId);
                         }
-                        $content  = "Amafaranga ntagomba kuba ubusa\n";
-                        $content .= "Murakoze!";
-                        $this->stop($content, $sessionId);
                         
                     }elseif ($values[2] == "2") {
                         $content  = "iyi service ntirigukora wongere ugerageze nyuma yamasaha 24\n";
