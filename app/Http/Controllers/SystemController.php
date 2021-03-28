cec9b31019ed47305d1df58b8a98763cc115abce
tinoxn <niyonshutivalentin@gmail.com>
2021-03-28 14:06:57 +0200

    data

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -34,14 +34,15 @@ class SystemController extends Controller
 
     }
     // DB::table('beneficiaries')->insert($customerArr);
-    // try {
+    try {
         DB::table('beneficiaries')->insert($customerArr);
         alert()->success('user importing returned with success','Success!');
         return Redirect::back(); 
-        // } catch (\Throwable $th) {
+        } catch (\Throwable $th) {
+            echo'error';
         //     alert()->error('unable to import data from file','Error!');
         //     return Redirect::back();
-        // }
+        }
         
     }
     public function csvToArray($filename = '', $delimiter = ';')
