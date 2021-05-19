b1f9911da94b114870d6524ec78569dc53eb68ac
tinoxn <niyonshutivalentin@gmail.com>
2021-05-19 15:02:21 +0200

    deb

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -63,6 +63,7 @@ class ClientController extends Controller
 
     public function actual()
     {
+        return Account::get();
         $get_actual = DB::table('beneficiaries')
                         ->join('administrative_locations','beneficiaries.location', '=','administrative_locations.id')
                         ->select(
@@ -278,6 +279,7 @@ class ClientController extends Controller
      */
     public function assignClient(Request $request)
     {
+        
         return $request;
         if($request->price == 0){
             alert()->success('this session has expired retry again!','Oops');
